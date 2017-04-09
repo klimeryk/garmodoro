47810218024f46c7670bc31cb4b2b29a96572011
Igor Klimer <igor@klimer.eu>
2017-04-09 13:28:30 +0200

    Add Makefiles for building
    Sorry, but while I love Eclipse for Java and C++ projects, it sucks for
    Connect IQ.

---
+++ properties.mk
@@ -0,0 +1,5 @@
+DEVICE = fenix3_hr
+SDK_HOME = /Users/klimeryk/work/connectiq/tools/connectiq-sdk-mac
+DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
+SUPPORTED_DEVICES_LIST = d2bravo d2bravo_titanium fenix3 fenix3_hr fenix5 fenix5s fenix5x fenixchronos fr230 fr235 fr630 fr735xt fr920xt vivoactive vivoactive_hr
+PRIVATE_KEY = /Users/klimeryk/work/connectiq/tools/developer_key.der
