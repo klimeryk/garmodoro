9b9185d762460dd9ac938e8fe3567d1e830d2455
Igor Klimer <igor@klimer.eu>
2017-04-22 23:33:07 +0200

    Add support for ALL THE DEVICES \o/
    Thanks to the layout calculations refactor, I just had to resize the
    launcher icon for some of the devices and that's all.

--- properties.mk
+++ properties.mk
@@ -1,5 +1,5 @@
 DEVICE = fenix3_hr
 SDK_HOME = /Users/klimeryk/work/connectiq/tools/connectiq-sdk-mac
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-SUPPORTED_DEVICES_LIST = d2bravo d2bravo_titanium fenix3 fenix3_hr fenix5 fenix5s fenix5x fenixchronos fr230 fr235 fr630 fr735xt fr920xt vivoactive vivoactive_hr
+SUPPORTED_DEVICES_LIST = d2bravo d2bravo_titanium fenix3 fenix3_hr fenix5 fenix5s fenix5x fenixchronos fr230 fr235 fr630 fr735xt fr920xt vivoactive vivoactive_hr epix edge_1000 edge_520 edge820 oregon7xx
 PRIVATE_KEY = /Users/klimeryk/work/connectiq/tools/developer_key.der
