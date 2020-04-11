b5afbff19d5d0ef221b3d984fa480dc583d129d0
Tom Gruen <github2014@thomas-gruen.de>
2020-04-11 18:07:06 +0200

    moved properties.mk to properties.mk.example added properties.mk to .gitignore resolves thomas-gruen/garmodoro #4

--- properties.mk
+++
@@ -1,4 +0,0 @@
-DEVICE = fenix3_hr
-SDK_HOME = /Users/klimeryk/work/connectiq/tools/connectiq-sdk-mac
-DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-PRIVATE_KEY = /Users/klimeryk/work/connectiq/tools/developer_key.der
