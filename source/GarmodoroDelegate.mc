klimeryk/garmodoro

commit 3a724b42d3ff78baf0b0008b50701aecdcf4f430

Optimize vibration
...that's an interesting commit title.
Another small optimization: instead of re-creating the same "vibe
profiles" every time there's a vibration need, let's create them once
and re-use. We have pleeeeeenty of memory, even on the oldest devices.

source/GarmodoroDelegate.mc
@@ -14,6 +14,8 @@ var pomodoroNumber as Number = 1;
 var isPomodoroTimerStarted as Boolean = false;
 var isBreakTimerStarted as Boolean = false;
 var needsClear as Boolean = true;
+var strongVibration as Attention.VibeProfile = new Attention.VibeProfile( 100, 1500);
+var softVibration as Attention.VibeProfile = new Attention.VibeProfile( 70, 1500 );
 
 (:newPropertiesApi)
 function getProperty(property as App.PropertyKeyType) as App.PropertyValueType {
@@ -25,9 +27,9 @@ function getProperty(property as App.PropertyKeyType) as App.PropertyValueType {
 	return App.getApp().getProperty( property );
 }
 
-function ping( dutyCycle as Number, length as Number ) as Void {
+function ping( vibeProfile as Attention.VibeProfile ) as Void {
 	if ( Attention has :vibrate ) {
-		Attention.vibrate( [ new Attention.VibeProfile( dutyCycle, length ) ] );
+		Attention.vibrate( [ vibeProfile ] );
 	}
 }
 
@@ -46,6 +48,8 @@ function resetMinutes() as Void {
 }
 
 class GarmodoroDelegate extends Ui.BehaviorDelegate {
+	hidden var tickVibration as Attention.VibeProfile = new Attention.VibeProfile( getProperty( "tickStrength" ) as Number, getProperty( "tickDuration" ) as Number );
+
 	function idleCallback() as Void {
 		Ui.requestUpdate();
 	}
@@ -60,7 +64,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 
 		if ( minutes == 0 ) {
 			play( Attention.TONE_LAP );
-			ping( 100, 1500 );
+			ping( strongVibration );
 			tickTimer.stop();
 			timer.stop();
 			isPomodoroTimerStarted = false;
@@ -79,7 +83,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 
 		if ( minutes == 0 ) {
 			play( Attention.TONE_INTERVAL_ALERT );
-			ping( 100, 1500 );
+			ping( strongVibration );
 			timer.stop();
 
 			needsClear = true;
@@ -97,7 +101,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 	}
 
 	function tickCallback() as Void {
-		ping( getProperty( "tickStrength" ) as Number, getProperty( "tickDuration" ) as Number );
+		ping( me.tickVibration );
 	}
 
 	function onBack() {
@@ -120,7 +124,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 		}
 
 		play( Attention.TONE_START );
-		ping( 75, 1500 );
+		ping( softVibration );
 		timer.stop();
 		resetMinutes();
 		timer.start( method( :pomodoroCallback ), MINUTE_IN_MILISECONDS, true );

source/StopMenuDelegate.mc
@@ -14,7 +14,7 @@ class StopMenuDelegate extends Ui.MenuInputDelegate {
 	function onMenuItem( item ) {
 		if ( item == :restart ) {
 			play( Attention.TONE_RESET );
-			ping( 50, 1500 );
+			ping( softVibration );
 
 			tickTimer.stop();
 			timer.stop();

commit a7eb25594d841fd3856778b9687f0a8e544c59f9

Fix app crash on devices not supporting vibration
Looks like a regression crept in
3a724b42d3ff78baf0b0008b50701aecdcf4f430.
The VibeProfile is apparently not defined on devices that don't support
:vibration. And it causes a crash.

To work around that, while still keeping the optimizations, let's keep
the type generic (Object) and initialize only (once) inside ping (which
has the :vibrate guard).

source/GarmodoroDelegate.mc
@@ -14,11 +14,13 @@ var pomodoroNumber as Number = 1;
 var isPomodoroTimerStarted as Boolean = false;
 var isBreakTimerStarted as Boolean = false;
 var needsClear as Boolean = true;
-var strongVibration as Attention.VibeProfile = new Attention.VibeProfile( 100, 1500);
-var softVibration as Attention.VibeProfile = new Attention.VibeProfile( 70, 1500 );
+
+var _strongVibration as Object? = null;
+var _softVibration as Object? = null;
+var _tickVibration as Object? = null;
 
 (:newPropertiesApi)
-function getProperty(property as App.PropertyKeyType) as App.PropertyValueType {
+function getProperty(property as String) as App.Properties.ValueType {
 	return App.Properties.getValue( property );
 }
 
@@ -27,10 +29,26 @@ function getProperty(property as App.PropertyKeyType) as App.PropertyValueType {
 	return App.getApp().getProperty( property );
 }
 
-function ping( vibeProfile as Attention.VibeProfile ) as Void {
-	if ( Attention has :vibrate ) {
-		Attention.vibrate( [ vibeProfile ] );
+function ping( profile as Symbol ) as Void {
+	if ( !( Attention has :vibrate ) ) {
+		return;
 	}
+
+	if ( _strongVibration == null ) {
+		_strongVibration = new Attention.VibeProfile( 100, 1500 );
+		_softVibration = new Attention.VibeProfile( 70, 1500 );
+		_tickVibration = new Attention.VibeProfile( getProperty( "tickStrength" ) as Number, getProperty( "tickDuration" ) as Number );
+	}
+
+	var vibe = ( profile == :strong ) ? _strongVibration
+	         : ( profile == :soft )   ? _softVibration
+	         : ( profile == :tick )   ? _tickVibration
+	         :                          null;
+	if ( vibe == null ) {
+		return;
+	}
+
+	Attention.vibrate( [ vibe as Attention.VibeProfile ] );
 }
 
 function play( tone as Attention.Tone ) as Void {
@@ -53,8 +71,6 @@ function resetMinutes() as Void {
 }
 
 class GarmodoroDelegate extends Ui.BehaviorDelegate {
-	hidden var tickVibration as Attention.VibeProfile = new Attention.VibeProfile( getProperty( "tickStrength" ) as Number, getProperty( "tickDuration" ) as Number );
-
 	function idleCallback() as Void {
 		requestViewUpdate( false );
 	}
@@ -69,7 +85,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 
 		if ( minutes == 0 ) {
 			play( Attention.TONE_LAP );
-			ping( strongVibration );
+			ping( :strong );
 			tickTimer.stop();
 			timer.stop();
 			isPomodoroTimerStarted = false;
@@ -87,7 +103,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 
 		if ( minutes == 0 ) {
 			play( Attention.TONE_INTERVAL_ALERT );
-			ping( strongVibration );
+			ping( :strong );
 			timer.stop();
 
 			isBreakTimerStarted = false;
@@ -104,7 +120,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 	}
 
 	function tickCallback() as Void {
-		ping( me.tickVibration );
+		ping( :tick );
 	}
 
 	function onBack() {
@@ -127,7 +143,7 @@ class GarmodoroDelegate extends Ui.BehaviorDelegate {
 		}
 
 		play( Attention.TONE_START );
-		ping( softVibration );
+		ping( :soft );
 		timer.stop();
 		resetMinutes();
 		timer.start( method( :pomodoroCallback ), MINUTE_IN_MILISECONDS, true );

source/StopMenuDelegate.mc
@@ -14,7 +14,7 @@ class StopMenuDelegate extends Ui.MenuInputDelegate {
 	function onMenuItem( item ) {
 		if ( item == :restart ) {
 			play( Attention.TONE_RESET );
-			ping( softVibration );
+			ping( :soft );
 
 			tickTimer.stop();
 			timer.stop();